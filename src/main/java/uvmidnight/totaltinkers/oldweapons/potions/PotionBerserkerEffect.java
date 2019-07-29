2808e76b56212c34e1349379ec30506d8378ddd1
UvMidnight <23361732+VoidSorc@users.noreply.github.com>
2019-07-29 15:46:52 -0400

    Negative resistance duration significantly increased
    Discourage players from using it as a quick toggle off

--- src/main/java/uvmidnight/totaltinkers/oldweapons/potions/PotionBerserkerEffect.java
+++ src/main/java/uvmidnight/totaltinkers/oldweapons/potions/PotionBerserkerEffect.java
@@ -19,7 +19,7 @@ public class PotionBerserkerEffect extends PotionEffect {
     public boolean onUpdate(EntityLivingBase entityIn) {
         if (entityIn.getHeldItemMainhand().getItem() instanceof WeaponBattleAxe) {
             entityIn.addPotionEffect(new PotionEffect(Potion.getPotionById(1), 30, OldWeapons.berserkerSpeed.getInt(), false, false));
-            entityIn.addPotionEffect(new PotionEffect(Potion.getPotionById(11), 30, OldWeapons.berserkerResistance.getInt(), false, false));
+            entityIn.addPotionEffect(new PotionEffect(Potion.getPotionById(11), 300, OldWeapons.berserkerResistance.getInt(), false, false));
             entityIn.addPotionEffect(new PotionEffect(Potion.getPotionById(8), 30, OldWeapons.berserkerJumpBoost.getInt(), false, false));
             entityIn.addPotionEffect(new PotionEffect(Potion.getPotionById(5), 30, OldWeapons.berserkerStrength.getInt(), false, false));
             entityIn.addPotionEffect(new PotionEffect(Potion.getPotionById(3), 30, OldWeapons.berserkerHaste.getInt(), false, false));
